e98c77f5c4615a0f8f32ee01bedb5d7f17a7ab9a
qyj <qyjbeijing@gmail.com>
2024-03-22 13:56:37 +0800

    MOD: 添加导出

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { serialize } from './serialize';
+export { deserialize } from './deserialize';
+export { SerializableContext } from './serializable-context';
+export { SerializableMeta, SerializableMode, SerializableFieldType, SerializableFieldMeta, SerializableParamMeta } from './serializable-meta';
+export { ISerialized, ClassConstructor, SerializableDataType } from './serializable-object';
+export { Serializable } from './decorator/serializable';
+export { SerializeParam } from './decorator/serialize-param';
+export { Ignore } from './decorator/ignore';
+export { Include } from './decorator/include';
